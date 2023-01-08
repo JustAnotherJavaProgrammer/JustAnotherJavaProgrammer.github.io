345febff1f2bf79084ad7fb4fefc458819c22f5d
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2023-01-08 03:55:50 +0100

    updated sveltekit

--- src/lib/components/Taglines.svelte
+++ src/lib/components/Taglines.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import {onMount} from "svelte";
-	import { browser } from '$app/env';
+	import { browser } from '$app/environment';
 	export let taglines: string[] = ['Hello world!'];
 	export let visibilityTime: number = 5;
 	const cycleDuration = visibilityTime + 1;
